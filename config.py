0765e1a7f8a1f110d05f190a2ff4210cf4f2160d
JOSE ANGEL IVAN RUBIANES SILVA <angelivan34@gmail.com>
2025-05-15 13:33:40 -0300

    Update config.py
    test internal url

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://app_user:6TwkGlkkbq7f71iNS6bgYKrTn4LA8LDA@dpg-d0j0os95pdvs739uh8gg-a/app_db_v4pz
-')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://app_user:6TwkGlkkbq7f71iNS6bgYKrTn4LA8LDA@dpg-d0j0os95pdvs739uh8gg-a/app_db_v4pz')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
